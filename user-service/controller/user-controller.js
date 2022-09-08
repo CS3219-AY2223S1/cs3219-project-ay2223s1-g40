617509ac71fedebbe18a0e81479a85dbe2cb3a21
branzuelajohn <branzuelajohn@gmail.com>
2022-09-08 15:14:37 +0800

    FR1.2 task

--- user-service/controller/user-controller.js
+++ user-service/controller/user-controller.js
@@ -1,21 +1,35 @@
-import { ormCreateUser as _createUser } from '../model/user-orm.js'
+import { ormCreateUser as _createUser } from "../model/user-orm.js";
+import { ormCheckUser as _checkUserbyUsername } from "../model/user-orm.js";
 
 export async function createUser(req, res) {
-    try {
-        const { username, password } = req.body;
-        if (username && password) {
-            const resp = await _createUser(username, password);
-            console.log(resp);
-            if (resp.err) {
-                return res.status(400).json({message: 'Could not create a new user!'});
-            } else {
-                console.log(`Created new user ${username} successfully!`)
-                return res.status(201).json({message: `Created new user ${username} successfully!`});
-            }
-        } else {
-            return res.status(400).json({message: 'Username and/or Password are missing!'});
-        }
-    } catch (err) {
-        return res.status(500).json({message: 'Database failure when creating new user!'})
+  try {
+    const { username, password } = req.body;
+    const checkDuplicate = await _checkUserbyUsername(username);
+    if (checkDuplicate.length > 0) {
+      console.log("The username already exists!");
+      return res.status(409).json({ message: "The username already exists!" });
     }
+    if (username && password) {
+      const resp = await _createUser(username, password);
+      console.log(resp);
+      if (resp.err) {
+        return res
+          .status(400)
+          .json({ message: "Could not create a new user!" });
+      } else {
+        console.log(`Created new user ${username} successfully!`);
+        return res
+          .status(201)
+          .json({ message: `Created new user ${username} successfully!` });
+      }
+    } else {
+      return res
+        .status(400)
+        .json({ message: "Username and/or Password are missing!" });
+    }
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: "Database failure when creating new user!" });
+  }
 }

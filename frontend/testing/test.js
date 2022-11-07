e12b890334dd851377674621094132b3c4ef4467
Alexander Er <aceszhenwei@yahoo.com>
2022-11-08 00:24:12 +0800

    Test Code (Incomplete)

---
+++ frontend/testing/test.js
@@ -0,0 +1,90 @@
+const io = require("socket.io-client");
+const ev = require("./utils/events");
+const logger = require("./utils/logger");
+
+// initSocket returns a promise
+// success: resolve a new socket object
+// fail: reject a error
+const initSocket = () => {
+  return new Promise((resolve, reject) => {
+    // create socket for communication
+    const port = process.env.PORT || 8001;
+    const socket = io(`localhost:${port}`, {
+      "reconnection delay": 0,
+      "reopen delay": 0,
+      "force new connection": true,
+    });
+
+    // define event handler for sucessfull connection
+    socket.on(ev.CONNECT, () => {
+      logger.info("connected");
+      resolve(socket);
+    });
+
+    // if connection takes longer than 5 seconds throw error
+    setTimeout(() => {
+      reject(new Error("Failed to connect wihtin 5 seconds."));
+    }, 5000);
+  });
+};
+
+// destroySocket returns a promise
+// success: resolve true
+// fail: resolve false
+const destroySocket = (socket) => {
+  return new Promise((resolve, reject) => {
+    // check if socket connected
+    if (socket.connected) {
+      // disconnect socket
+      logger.info("disconnecting...");
+      socket.disconnect();
+      resolve(true);
+    } else {
+      // not connected
+      logger.info("no connection to break...");
+      resolve(false);
+    }
+    logger.info("---------------------------------");
+  });
+};
+
+describe("test suit: Echo", () => {
+  test("test: ECHO", async () => {
+    // create socket for communication
+    const socketClient = await initSocket();
+
+    // create new promise for server response
+    const serverResponse = new Promise((resolve, reject) => {
+      // define a handler for the test event
+      socketClient.on(ev.res_ECHO, (data4Client) => {
+        //process data received from server
+        const { message } = data4Client;
+        logger.info("Server says: " + message);
+
+        // destroy socket after server responds
+        destroySocket(socketClient);
+
+        // return data for testing
+        resolve(data4Client);
+      });
+
+      // if response takes longer than 5 seconds throw error
+      setTimeout(() => {
+        reject(new Error("Failed to get reponse, connection timed out..."));
+      }, 5000);
+    });
+
+    // define data 4 server
+    const data4Server = { message: "CLIENT ECHO" };
+
+    // emit event with data to server
+    logger.info("Emitting ECHO event");
+    socketClient.emit(ev.req_ECHO, data4Server);
+
+    // wait for server to respond
+    const { status, message } = await serverResponse;
+
+    // check the response data
+    expect(message).toBe("a user connected");
+  });
+});
---
+++ frontend/testing/utils/events.js
@@ -0,0 +1,13 @@
+module.exports = {
+    CONNECT: "connect",
+  
+    DISCONNECT: "disconnect",
+  
+    // requests
+    req_ECHO: "#echo",
+    req_BELLO: "#bello",
+  
+    // responses
+    res_ECHO: "@echo",
+    res_BELLO: "@bello"
+  };
